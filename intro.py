815e3cdf3a71f81bb8c3fcc1601556de12d6f833
Hugo Souto <hugomsouto@gmail.com>
2023-06-16 23:25:38 -0300

    Initiate file

---
+++ intro.py
@@ -0,0 +1,17 @@
+# Load libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read in the data
+data = pd.read_csv('data/socr-height-weight.csv')
+# students
+
+# Write equation for a line
+# y = 9.85 * students.hours_studied + 43
+
+# Create the plot here: 
+plt.scatter(data.Weight(Pounds), students.weight)
+
+#plt.plot(students.hours_studied, y)
+plt.show()
