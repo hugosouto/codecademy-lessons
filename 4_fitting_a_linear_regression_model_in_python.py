4936da83ea5458b525d2a69862c81d473ba3516c
Hugo Souto <hugomsouto@gmail.com>
2023-06-18 21:43:35 -0300

    Create file

---
+++ 4_fitting_a_linear_regression_model_in_python.py
@@ -0,0 +1,14 @@
+# Load libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+# Read in the data
+students = pd.read_csv('test_data.csv')
+
+# Create the model here:
+
+# Fit the model here:
+
+# Print the coefficients here:
